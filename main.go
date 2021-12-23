9451c5721691c344af2db7b54c1079026b592fd4
SergioNicolasSotelo <sergio.sotelo@mercadolibre.com>
2021-12-23 12:02:17 -0300

    arreglado error en get productos

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Productos struct {
+	Id            int    `json:"id"`
+	Nombre        string `json:"nombre"`
+	Color         string `json:"color"`
+	Precio        int    `json:"precio"`
+	Stock         int    `json:"stock"`
+	Codigo        string `json:"codigo"`
+	Publicado     bool   `json:"publicado"`
+	FechaCreacion string `json:"fecha_creacion"`
+}
 
 func main() {
 
 	router := gin.Default()
 
-	router.GET("/hola", func(c *gin.Context) {
+	router.GET("/hola/:nombre", func(c *gin.Context) {
+		nombre := c.Param("nombre")
 		c.JSON(200, gin.H{
-			"message": "hola",
+			"message": "Hola, " + nombre,
 		})
+	})
+	/*-------------------------------------------------*/
+
+	prods, err := ioutil.ReadFile("products.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	productos := Productos{}
+	err = json.Unmarshal(prods, &productos)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router.GET("/productos", func(c *gin.Context) {
+		c.JSON(200, productos)
 	})
 
 	router.Run()
